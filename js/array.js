5f472282bc6d3ac7c860df5b3c96e422c01fe80d
sumi <sumi@sumi.space>
2025-05-17 13:48:27 +0900

    shuffle

--- js/array.js
+++ js/array.js
@@ -1,1 +1,5 @@
-export const chooseRandom = (ones) => ones[Math.random() * ones.length];
+export const chooseRandom = (they) => they[Math.random() * they.length];
+export const shuffle = (they) => they
+    .map((it) => ({ it, value: Math.random() }))
+    .sort((a, b) => a.value - b.value)
+    .map(({ it }) => it);
--- ts/array.ts
+++ ts/array.ts
@@ -1,2 +1,8 @@
-export const chooseRandom = <A>(ones: A[]): A =>
-  ones[Math.random() * ones.length]!;
+export const chooseRandom = <A>(they: A[]): A =>
+  they[Math.random() * they.length]!;
+
+export const shuffle = <A>(they: A[]): A[] =>
+  they
+    .map((it) => ({ it, value: Math.random() }))
+    .sort((a, b) => a.value - b.value)
+    .map(({ it }) => it);
