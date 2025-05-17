1f6c6bdb6f6ace2e788fb8341d0c6d13dc129b5e
sumi <sumi@sumi.space>
2025-05-17 20:11:48 +0900

    random

---
+++ ts/random.ts
@@ -0,0 +1,61 @@
+export class XorShift {
+  static until: number = 4294967296;
+
+  ns: Uint32Array;
+
+  constructor(seed: number = 88675123) {
+    this.ns = new Uint32Array(4);
+    this.ns[0] = 123456789;
+    this.ns[1] = 362436069;
+    this.ns[2] = 521288629;
+    this.ns[3] = seed;
+  }
+
+  // XorShift
+  next() {
+    const e = this.ns[0] ^ (this.ns[0] << 11);
+    this.ns[0] = this.ns[1];
+    this.ns[1] = this.ns[2];
+    this.ns[2] = this.ns[3];
+    this.ns[3] = this.ns[3] ^ (this.ns[3] >>> 19) ^ (e ^ (e >>> 8));
+    return this.ns[3];
+  }
+
+  nextUnit() {
+    return this.next() / XorShift.until;
+  }
+
+  nextBetween(since: number, until: number) {
+    return this.nextUnit() * (until - since) + since;
+  }
+
+  lottery = <A>(they: A[]): A => they[this.nextUnit() * they.length]!;
+
+  shuffle = <A>(they: A[]): A[] =>
+    they
+      .map((it) => ({ it, value: this.nextUnit() }))
+      .sort((a, b) => a.value - b.value)
+      .map(({ it }) => it);
+}
+
+if (false) {
+  const random = new XorShift();
+
+  for (let i = 0; i < 1000000; i++)
+    console.assert(0 <= random.nextUnit() && random.nextUnit() < 1);
+
+  let sum = 0;
+  let min = Number.MAX_SAFE_INTEGER;
+  let max = 0;
+  let n = 1000000;
+  for (let i = 0; i < n; i++) {
+    const r = random.nextUnit();
+    sum += r;
+    if (max < r) max = r;
+    if (r < min) min = r;
+  }
+
+  console.log("min", min);
+  console.log("max", max);
+  console.log("mean", sum / n);
+}
