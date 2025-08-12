a05d00abf59ec07480b9350f5e6729c26a042961
sumi <sumi@sumi.space>
2025-08-12 23:12:17 +0900

    update

--- ts/develop.ts
+++
@@ -1,4 +0,0 @@
-export const log = <A>(it: A): A => {
-  console.log(it);
-  return it;
-};
--- ts/html.ts
+++
@@ -1,8 +0,0 @@
-export const assignStyle = (
-  parent: HTMLElement,
-  query: string,
-  styles: Object
-) => {
-  for (const element of parent.querySelectorAll<HTMLElement>(query))
-    Object.assign(element.style, styles);
-};
